5a5998c1c7ebec5f9a1a16f2c54d4176098804a3
Nathan Zadoks <nathan@nathan7.eu>
2013-02-08 19:29:41 +0100

    style adjustments

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var fs = require('fs');
-var path = require('path');
-
-module.exports = trail;
+module.exports = trail
+var fs = require('fs')
+  , path = require('path')
 
 function trail(paths, fname, cb) {
-    var pathlist = paths.map(function(dirname) {
-        return path.join(dirname, fname);
-    });
+  var pathlist = paths.map(function(dirname) {
+    return path.join(dirname, fname)
+  })
 
-    (function checkPath(pathname) {
-        (fs.exists || path.exists)(pathname, function(exists) {
-            if (exists) return cb(null, pathname);
-            if (!pathlist.length) return cb(null, null);
-            checkPath(pathlist.shift());
-        });
-    })(pathlist.shift());
+  ;(function checkPath(pathname) {
+    (fs.exists || path.exists)(pathname, function(exists) {
+      if (exists)
+          return cb(null, pathname)
+      if (!pathlist.length)
+          return cb(null, null)
+      checkPath(pathlist.shift())
+    })
+  })(pathlist.shift())
 }
 
-trail.bail = bail;
-
+trail.bail = bail
 function bail(paths, fname, cb) {
-    trail(paths, fname, function(err, fpath) {
-        if (!fpath) return cb('not found');
-        cb(null, fpath);
-    });
+  trail(paths, fname, function(err, fpath) {
+    if (!fpath) return cb('not found')
+    cb(null, fpath)
+  })
 }
--- test.js
+++ test.js
@@ -1,49 +1,45 @@
-var path = require('path');
-var fs = require('fs');
-var should = require('should');
+var path = require('path')
+  , fs = require('fs')
+  , should = require('should')
+  , trail = require('./index')
 
-var trail = require('./index');
-
-var testdir = __dirname + '/testdata';
-var testdirs = ['a', 'b', 'c'].map(function(subdir) {
-    return path.join(testdir, subdir);
-});
-var testfile = 'file';
+var testdir = __dirname + '/testdata'
+  , testdirs = ['a', 'b', 'c'].map(function(subdir) { return path.join(testdir, subdir) })
+  , testfile = 'file'
 
 
 describe('trail', function() {
     it('should find files that exist', function(done) {
         trail(testdirs, testfile, function(err, found) {
-            should.not.exist(err);
-            found.should.equal(path.join(testdirs[1], testfile));
-            (fs.existsSync || path.existsSync)(found).should.equal(true);
-            done();
-        });
-    });
+            should.not.exist(err)
+            found.should.equal(path.join(testdirs[1], testfile))
+            ;(fs.existsSync || path.existsSync)(found).should.equal(true)
+            done()
+        })
+    })
     it('should return null on files that don\'t exist', function(done) {
         trail(testdirs, 'does-not-exist', function(err, found) {
-            should.not.exist(err);
-            should.not.exist(found);
-            done();
-        });
-    });
-});
+            should.not.exist(err)
+            should.not.exist(found)
+            done()
+        })
+    })
+})
 
 describe('bail', function() {
     it('should find files that exist', function(done) {
         trail.bail(testdirs, testfile, function(err, found) {
-            should.not.exist(err);
-            found.should.equal(path.join(testdirs[1], testfile));
-            (fs.existsSync || path.existsSync)(found).should.equal(true);
-            done();
-        });
-    });
+            should.not.exist(err)
+            found.should.equal(path.join(testdirs[1], testfile))
+            ;(fs.existsSync || path.existsSync)(found).should.equal(true)
+            done()
+        })
+    })
     it('should pass an error on files that don\'t exist', function(done) {
         trail.bail(testdirs, 'does-not-exist', function(err, found) {
-            err.should.equal('not found');
-            should.not.exist(found);
-            done();
-        });
-
-    });
-});
+            err.should.equal('not found')
+            should.not.exist(found)
+            done()
+        })
+    })
+})
